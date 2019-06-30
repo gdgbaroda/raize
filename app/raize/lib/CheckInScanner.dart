7126c89edc520abc1ade92f267ea8a8c7bf15c41
prachidurge <prachi.durge@gmail.com>
2019-06-30 16:05:19 +0530

    QRScan API scaffold added.

--- app/raize/lib/CheckInScanner.dart
+++ app/raize/lib/CheckInScanner.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:raize/QRScanAPI.dart';
 import 'package:simple_permissions/simple_permissions.dart';
 
 void main() {
@@ -14,64 +15,87 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _reader='';
-  Permission permission= Permission.Camera;
+  String _reader = '';
+  Color _resultColor;
+  Permission permission = Permission.Camera;
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      color: Colors.pinkAccent,
-      home:new Scaffold(
-        appBar: new AppBar(title: new Text("Scanner"),backgroundColor: Colors.orange,),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Scanner"),
+        ),
         body: new Column(
           children: <Widget>[
-
             new Padding(
               padding: const EdgeInsets.symmetric(vertical: 10.0),
             ),
             new RaisedButton(
-
-              splashColor: Colors.pinkAccent,
-              color: Colors.red,
-              child: new Text("Scan",style: new TextStyle(fontSize: 20.0,color: Colors.white),),
+              child: new Text(
+                "Check In",
+                style: new TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
               onPressed: scan,
             ),
-            new Padding(padding: const EdgeInsets.symmetric(vertical: 10.0), ),
-            new Text('$_reader',softWrap: true, style: new TextStyle(fontSize: 30.0,color: Colors.blue),),
-
+            new Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+            ),
+            new Text(
+              '$_reader',
+              softWrap: true,
+              style: new TextStyle(fontSize: 20.0, color: _resultColor ),
+            ),
           ],
         ),
       ),
     );
   }
 
-
-
   requestPermission() async {
-    PermissionStatus result = await SimplePermissions.requestPermission(permission);
-    setState(()=> new SnackBar
-      (backgroundColor: Colors.red,content: new Text(" $result"),),
-
+    PermissionStatus result =
+        await SimplePermissions.requestPermission(permission);
+    setState(
+      () => new SnackBar(
+            backgroundColor: Colors.red,
+            content: new Text(" $result"),
+          ),
     );
   }
+
   scan() async {
     try {
-      String reader= await BarcodeScanner.scan();
+      String reader = await BarcodeScanner.scan();
 
       if (!mounted) {
         return;
       }
 
-      setState(() => this._reader=reader);
-    } on PlatformException catch(e) {
-      if(e.code==BarcodeScanner.CameraAccessDenied) {requestPermission();}
-      else{setState(()=> _reader = "unknown exception $e");}
-    }on FormatException{
-      setState(() => _reader = 'null (User returned using the "back"-button before scanning anything. Result)');
+      bool userAuthenticated = await QRScanAPI.checkIn(reader);
+
+      setState(() => showScanResults(userAuthenticated));
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        requestPermission();
+      } else {
+        setState(() => _reader = "unknown exception $e");
+      }
+    } on FormatException {
+      setState(() => _reader =
+          'null (User returned using the "back"-button before scanning anything. Result)');
     } catch (e) {
       setState(() => _reader = 'Unknown error: $e');
     }
-
   }
 
+  showScanResults(bool userAuthenticated) {
+    if (userAuthenticated) {
+      this._reader = "Approved..";
+      this._resultColor = Colors.green;
+    } else {
+      this._reader = "Please contact event orgnaizer!";
+      this._resultColor = Colors.red;
+    }
+  }
 }
---
+++ app/raize/lib/QRScanAPI.dart
@@ -0,0 +1,11 @@
+class QRScanAPI{
+  static Future checkIn(String reader) async {
+    try {
+     // await
+      //TODO: API call here
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
