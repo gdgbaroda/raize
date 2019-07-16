06e4db56e12432fa01ee22e6b747d3152304d8f2
prachidurge <prachi.durge@gmail.com>
2019-07-16 12:35:37 +0530

    Phase 1: Added dummy json for event-list screen

--- app/raize/lib/EventListWidget.dart
+++ app/raize/lib/EventListWidget.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:raize/models/EventModel.dart';
 import 'package:http/http.dart' as http;
 import 'package:raize/EventDetailsWidget.dart';
+
+import 'models/EventListModel.dart';
 
 class EventListWidget extends StatefulWidget {
   static String tag = 'eventList-screen';
@@ -12,13 +16,14 @@ class EventListWidget extends StatefulWidget {
 }
 
 class _EventListWidget extends State<EventListWidget> {
-  List<EventModel> _items = new List();
+ // List<EventModel> _items = new List();
+  List<EventListDummyModel> _items = new List();
 
 
   //add an item to the list
   void _addItem(item) {
     setState(() {
-      _items.add(EventModel.fromJson(item));
+      _items.add(EventListDummyModel.fromJson(item));
     });
   }
 
@@ -26,54 +31,53 @@ class _EventListWidget extends State<EventListWidget> {
   void initState() {
     super.initState();
     //this will be called at the start of the activity,it will add dummy data to our list
-    for (var i =0;i<15;i++){
-
+    for (var i = 0; i < 15; i++) {
       _addItem({
         "id": 1,
         "banner": "https://dart-code.gallerycdn.vsassets.io/extensions/dart-code/flutter/3.2.0/1561724669242/Microsoft.VisualStudio.Services.Icons.Default",
-        "title":"Learning flutter: Rx dart",
-        "description":"this event will be about learning how to implement rx in dart project",
-        "hosts":[
+        "title": "Learning flutter: Rx dart",
+        "description": "this event will be about learning how to implement rx in dart project",
+        "hosts": [
           {
             "fullname": "kushal dave",
-            "email":"kushaldave2015@gmail.com"
+            "email": "kushaldave2015@gmail.com"
           },
           {
             "fullname": "kushal",
-            "email":"kushaldave2011@gmail.com"
+            "email": "kushaldave2011@gmail.com"
           }
         ],
-        "attendees":[
+        "attendees": [
           {
             "fullname": "Jane Doe",
-            "email":"jane.doe@gmail.com",
-            "state":[
+            "email": "jane.doe@gmail.com",
+            "state": [
               {
-                "value":"Invited",
-                "updatedOn":"01/01/2019"
+                "value": "Invited",
+                "updatedOn": "01/01/2019"
               },
               {
-                "value":"Invited",
-                "updatedOn":"01/01/2019"
+                "value": "Invited",
+                "updatedOn": "01/01/2019"
               }
             ]
           }
         ],
         "venue": {
-          "address":"xya",
+          "address": "xya",
           "city": "vadodara",
-          "state":"gujarat",
-          "country":"INDIA",
-          "pinCode":123456
+          "state": "gujarat",
+          "country": "INDIA",
+          "pinCode": 123456
         },
-        "duration":{
-          "start":{
-            "date":"01/01/2019",
-            "time":"10:00"
+        "duration": {
+          "start": {
+            "date": "01/01/2019",
+            "time": "10:00"
           },
-          "end":{
-            "date":"01/01/2019",
-            "time":"12:00"
+          "end": {
+            "date": "01/01/2019",
+            "time": "12:00"
           }
         },
         "isActive": true
@@ -83,14 +87,14 @@ class _EventListWidget extends State<EventListWidget> {
 
 
   //creates view for each item in listview
-  Widget _createEventItem(BuildContext context, EventModel eventModel) {
+  Widget _createEventItem(BuildContext context, EventListDummyModel eventModel) {
     return new GestureDetector( //listens for on tap
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(
-           builder: (context) => EventDetailsWidget(eventModel:eventModel),
-              //builder: (context) => new EventDetailsWidget()
+            //builder: (context) => EventDetailsWidget(eventModel: eventModel),
+            builder: (context) => new EventDetailsWidget()
           ),
         );
       },
@@ -100,25 +104,27 @@ class _EventListWidget extends State<EventListWidget> {
               padding: new EdgeInsets.all(8.0),
               child: new Row(
                 children: <Widget>[
-                  new Image.network(eventModel.banner,
+                  new Image.network("test",//eventModel.events[1].title,
                       height: 80.0, width: 80.0, fit: BoxFit.fitHeight),
                   Expanded(
                       child: Container(
                         height: 80.0,
                         margin: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: _createEventItemDescriptionSection(context, eventModel),
+                        /*child: _createEventItemDescriptionSection(
+                            context, eventModel.events[1]),*/
                       )),
                 ],
               )),
 
-          new Divider(height: 15.0,color: Colors.black,),
+          new Divider(height: 15.0, color: Colors.black,),
 
         ],
       ),
     );
   }
 
-  Widget _createEventItemDescriptionSection(BuildContext context, EventModel eventModel) {
+  Widget _createEventItemDescriptionSection(BuildContext context,
+      EventModel eventModel) {
     return Column(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.start,
@@ -159,7 +165,7 @@ class _EventListWidget extends State<EventListWidget> {
         child: new Column(
           children: <Widget>[
             new Expanded(
-              child: Card(
+              /*child: Card(
                 child: ListView.builder(
                   padding: new EdgeInsets.all(8.0),
                   itemCount: _items.length,
@@ -168,6 +174,26 @@ class _EventListWidget extends State<EventListWidget> {
                   },
                 ),
               ),
+            ),*/
+
+            child: new FutureBuilder(
+                future: DefaultAssetBundle
+                    .of(context)
+                    .loadString('assets/raize-dummy-events-list.json'),
+                builder: (context, snapshot) {
+                  // Decode the JSON
+                  var new_data = json.decode(snapshot.data.toString());
+
+                  return new ListView.builder(
+                    // Build the ListView
+                    padding: new EdgeInsets.all(8.0),
+                    itemBuilder: (BuildContext context, int index) {
+
+                      return  _createEventItem(context, _items[index]);
+                    },
+                    itemCount: new_data == null ? 0 : new_data.length,
+                  );
+                })
             )
           ],
         ),
