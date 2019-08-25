566572c5df30fa0c7c492b32ba81842fc1b0ac04
Hardik Mistry <mistryhardik.05@gmail.com>
2019-08-25 22:52:21 +0530

    - Handle the meetup api parser object changes - Fetch and return object with host and venue information for each event - Handle asyn/await calls by function

--- api/routes/meetup-parsed.js
+++ api/routes/meetup-parsed.js
@@ -24,8 +24,34 @@ router.get('/auth',
         return res.json(req.user);
     });
 
-//for authentication on meetup callback
+/**
+ * @swagger
+ * /callback:
+ *    get:
+ *      description: Handle callback for the authorized meetup user
+ */
 router.get('/callback', function (req, res) {
+
+    if (req.query == {})
+        return res.json(false);
+
+    if (req.query.code == undefined)
+        return res.json(false);
+
+    // ensure the query parameter has a value
+    if (req.query.code != '')
+        return res.json(true);
+
+    return res.json(false);
+});
+
+/**
+ * @swagger
+ * /callback/token:
+ *    get:
+ *      description: Explicitly obtain access token for the meetup user
+ */
+router.get('/callback/token', function (req, res) {
     axios.post('https://secure.meetup.com/oauth2/access', {}, {
         params: {
             client_id: config.MEETUP_CLIENT_ID,
@@ -37,7 +63,6 @@ router.get('/callback', function (req, res) {
     }).then((data) => {
         req.flash("user", data.data);
         req.session.user = data.data;
-
         return res.json(data.data)
 
     }).catch((error) => {
@@ -47,46 +72,54 @@ router.get('/callback', function (req, res) {
 
 //fetching groups of authenticated user
 router.get('/groups', async function (req, res) {
-    let user = req.flash("user");
-    // let access_token = user[0].access_token;
-    let access_token = req.session.user.access_token;
+
+    var bearerHeader = req.headers["authorization"];
 
     let parsed_group_data = [];
+    let parsed_events_data = [];
+    let i = 0;
+
     await axios.get('https://api.meetup.com/self/groups', {
         headers: {
-            Authorization: "Bearer " + access_token,
+            Authorization: bearerHeader
         }
-    }).then((data) => {
+    }).then(async (groups) => {
 
-        data.data.forEach(all_data => {
+        for(let group of groups.data)
+        {
+            let events = await GetEvents(group.urlname, bearerHeader)
 
+            console.log('Events: ' + JSON.stringify(events))
 
-            parsed_group_data.push({
-                title: all_data.name,
-                source: "meetup",
-                location: all_data.localized_location,
-                events: events_id,
-                lastUpdatedOn: all_data.updated,
-                isActive: all_data.status,
-            });
-        });
+            if(JSON.stringify(events) !== '[]')
+            {
+                parsed_group_data.push({
+                    title: group.name,
+                    source: "meetup",
+                    location: group.localized_location,
+                    events: events,
+                    lastUpdatedOn: group.updated,
+                    isActive: group.status,
+                });
+            }
+        }
+
+        return res.json({groups: parsed_group_data})
 
     }).catch((error) => {
         console.error(error)
     });
 
-    return res.json(parsed_group_data)
 });
 
 //fetching events of a group
 router.get('/:urlname/', function (req, res) {
-    let user = req.flash("user");
-    // let access_token = user[0].access_token;
-    let access_token = req.session.user.access_token;
+    
+    var bearerHeader = req.headers["authorization"];
 
     axios.get(`https://api.meetup.com/${req.params.urlname}/events`, {
         headers: {
-            Authorization: "Bearer " + access_token,
+            Authorization: bearerHeader
         }
     }).then((data) => {
         return res.json(data.data)
@@ -97,13 +130,12 @@ router.get('/:urlname/', function (req, res) {
 
 //fetching rsvp of an event
 router.get('/:urlname/:event_id/rsvp', function (req, res) {
-    let user = req.flash("user");
-// let access_token = user[0].access_token;
-    let access_token = req.session.user.access_token;
+
+    var bearerHeader = req.headers["authorization"];
 
     axios.get(`https://api.meetup.com/${req.params.urlname}/events/${req.params.event_id}/rsvps`, {
         headers: {
-            Authorization: "Bearer " + access_token,
+            Authorization: bearerHeader
         }
     }).then((data) => {
         return res.json(data.data)
@@ -113,5 +145,132 @@ router.get('/:urlname/:event_id/rsvp', function (req, res) {
 
 });
 
+async function GetEvents(urlname, tokenHeader) {
+    try 
+    {
+        let parsed_events = [];
+
+        const response = await axios.get(`https://api.meetup.com/${urlname}/events`, {
+                            headers: {
+                                Authorization: tokenHeader
+                            }
+                        });
+
+        if(JSON.stringify(response.data) !== '[]')
+        {
+            for(let event of response.data)
+            {
+                let events_by_host = await GetEventsByHost(event.id, urlname, tokenHeader)
+
+                console.log('event ' + JSON.stringify(event));
+
+                if(JSON.stringify(events_by_host) !== '{}')
+                {
+                    parsed_events.push({
+
+                        title: event.name,
+                        host:true,
+                        description: event.description,
+                        venue: event.venue,
+                        duration: event.duration,
+                        isActive: true
+    
+                    })
+                }
+                else
+                {
+                    parsed_events.push({
+
+                        title: event.name,
+                        host:false,
+                        description: event.description,
+                        venue: event.venue,
+                        duration: event.duration,
+                        isActive: true
+    
+                    })
+                }
+            }
+        }
+        else
+        {
+            console.log('No events for ' + urlname);
+        }
+
+        return parsed_events;
+
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function GetEventsByHost(event_id, urlname, tokenHeader) {
+    try 
+    {
+        let filtered_events = {};
+
+        const response = await axios.get(`https://api.meetup.com/${urlname}/events/${event_id}/rsvps`, {
+                        headers: {
+                            Authorization: tokenHeader
+                        }
+                    });
+
+        if(JSON.stringify(response.data) !== '[]')
+        {
+            // get profile for the logged in user
+            let profile = await GetProfile(tokenHeader)
+
+            // filter by member id
+            var result = response.data.filter(function(obj, index){
+                return obj.member.id === profile.id;
+            })
+
+            if(JSON.stringify(result) !== '[]')
+            {
+                for(let event of result)
+                {
+                    if(event.member.event_context.host === true)
+                    {
+                        filtered_events = {
+                            id:event.event.id,
+                            host:event.member.event_context.host
+                        }
+                    }
+                    else
+                    {
+                        console.log('Is event host: ' + JSON.stringify(event.member.event_context.host))
+                    }
+                }
+            }
+        }
+        else
+        {
+            console.log('No events for ' + urlname);
+        }
+
+        return filtered_events;
+
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function GetProfile(tokenHeader) {
+    try 
+    {
+        const response = await axios.get(`https://api.meetup.com/members/self`, {
+            headers: {
+                Authorization: tokenHeader
+            }
+        });
+
+        console.log('Member profile: ' + JSON.stringify(response.data))
+
+        return response.data;
+
+    } catch (error) {
+      console.error(error);
+    }
+}
 
 module.exports = router;
