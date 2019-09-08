497d36f3b1cef79e575fbe1a05651f06e82f3098
Dhruvil Thaker <coffeecompile29@gmail.com>
2019-09-08 15:26:48 +0530

    -fixed csv creator and add row on status true of ticket

--- api/helpers/csv-writer.js
+++ api/helpers/csv-writer.js
@@ -1,11 +1,15 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+var json2csv = require('json2csv');
+var newLine = "\r\n";
 var methods = {};
 
 methods.CreateCSV = async function (data) {
     let today = new Date();
     let date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
     let path = `./data/${date}.csv`;
     if (!fs.existsSync(path)) {
         const csvWriter = createCsvWriter({
@@ -18,8 +22,33 @@ methods.CreateCSV = async function (data) {
                 {id: 'check_in', title: 'CHECK IN'}
             ]
         });
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var dateTime = date + ' ' + time;
+
+        const records = [
+            {
+                event: data['payment']['link_title'],
+                payment_id: data['payment']['payment_id'],
+                name: data['payment']['buyer_name'],
+                email: data['payment']['buyer_email'],
+                check_in: dateTime,
+            },
+        ];
+
+        csvWriter.writeRecords(records)       // returns a promise
+            .then(() => {
+                console.log('...Done');
+            });
+    } else {
+        const csvWriter = createCsvWriter({
+            path: path,
+            append: true,
+            header: [
+                {id: 'event', title: 'EVENT'},
+                {id: 'payment_id', title: 'PAYMENT ID'},
+                {id: 'name', title: 'NAME'},
+                {id: 'email', title: 'EMAIL'},
+                {id: 'check_in', title: 'CHECK IN'}
+            ]
+        });
         const records = [
             {
                 event: data['payment']['link_title'],
