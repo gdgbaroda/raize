a58d959c17c9352d5aa4fa783c3ebedbce4f0f51
dkots111 <39180067+dkots111@users.noreply.github.com>
2019-09-15 17:01:11 +0530

    maps-url-change

--- app/raize/lib/detail_widget.dart
+++ app/raize/lib/detail_widget.dart
@@ -10,9 +10,10 @@ import 'models/event_model.dart';
 class DetailWidget extends StatefulWidget {
   final EventModel eventModel;
 
+
   DetailWidget({@required this.eventModel});
   @override
-  _MyAppState createState() => new _MyAppState(eventModel);
+  _MyAppState createState() => _MyAppState(eventModel);
 }
 
 class _MyAppState extends State<DetailWidget> {
@@ -27,74 +28,93 @@ class _MyAppState extends State<DetailWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return new Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-//        new Image.network(
-//          eventModel.banner,//todo change it from new data model
-//          height: 150,
-//          width: double.infinity,
-//          fit: BoxFit.fitWidth,
-//        ),
         Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: new Text(
-            eventModel.description,
-            style: TextStyle(fontSize: 20),
+          padding: EdgeInsets.all(20.0),
+          child: Text(
+            eventModel.title,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25
+            ),
           ),
         ),
-//        Padding(
-//          padding: const EdgeInsets.all(20.0),
-//          child: new Text(
-//            eventModel.duration.start.date,
-//            style: TextStyle(fontSize: 20),
-//          ),
-//        ),
-        const SizedBox(height: 90.0),
-        new Container(
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Text(
+            eventModel.description,
+            style: TextStyle(
+                fontSize: 20
+            ),
+          ),
+        ),
+        SizedBox(height: 90.0),
+        Container(
           alignment: Alignment.bottomCenter,
-          child: new SizedBox(
+          child: (eventModel.host)? SizedBox(
             width: 350.0,
             height: 50.0,
-            child: new RaisedButton(
-              child: new Text('Navigation'),
+            child: RaisedButton(
+              child:
+               Text('Scan',
+                 style: TextStyle(
+                     color: Colors.white
+                 ),
+               ) ,
+              color: Colors.blue,
+              onPressed:  () => _scanQr(true),
+            ),
+          ):SizedBox(height: 0.0),
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: (eventModel.host)? SizedBox(
+            width: 350.0,
+            height: 50.0,
+            child: RaisedButton(
+              color: Colors.blue,
+              child: Text('Swag',
+              style: TextStyle(
+                color: Colors.white
+              ),
+              ),
+              onPressed: () => _scanQr(false),
+            ),
+          ):SizedBox(height:0.0),
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: 350.0,
+            height: 50.0,
+            child: OutlineButton(
+              color: Colors.white,
+              borderSide: BorderSide(color: Colors.blue),
+              child: Text('Navigation',
+                style: TextStyle(
+                    color: Colors.blue
+                ),),
               onPressed: _launchMapsUrl,
             ),
           ),
         ),
-        const SizedBox(height: 10.0),
-        Visibility(
-            visible: eventModel.host,
-            child: Column(children: [
-              new SizedBox(
-                width: 350.0,
-                height: 50.0,
-                child: new RaisedButton(
-                  child: Text('Scan'),
-                  onPressed: scan,
-                ),
-              ),
-              const SizedBox(height: 30.0),
-              new SizedBox(
-                width: 350.0,
-                height: 50.0,
-                child: new RaisedButton(
-                  child: Text('swag'),
-                  onPressed: scan,
-                ),
-              )
-            ])),
-        new Container(
-          child: new Row(
-            children: <Widget>[
-              new Text(
-                '$_reader',
-                softWrap: true,
-                style: new TextStyle(fontSize: 20.0, color: _resultColor),
-              ),
-            ],
-          ),
+        SizedBox(height: 30.0),
+        Container(
+        child : Row(
+          children: <Widget>[
+            Text(
+              '$_reader',
+              softWrap: true,
+              style: TextStyle(fontSize: 20.0, color: _resultColor),
+            ),
+
+          ],
+        ),
         )
       ],
     );
@@ -104,14 +124,14 @@ class _MyAppState extends State<DetailWidget> {
     PermissionStatus result =
         await SimplePermissions.requestPermission(permission);
     setState(
-      () => new SnackBar(
+      () => SnackBar(
         backgroundColor: Colors.red,
-        content: new Text(" $result"),
+        content: Text(" $result"),
       ),
     );
   }
 
-  scan() async {
+  _scanQr(bool) async {
     try {
       String qrData = await BarcodeScanner.scan();
 
@@ -122,7 +142,7 @@ class _MyAppState extends State<DetailWidget> {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => QRScanState(qrData: qrData),
+          builder: (context) => QRScanState(qrData: qrData,isRegistration: bool),
         ),
       );
     } on PlatformException catch (e) {
@@ -132,18 +152,18 @@ class _MyAppState extends State<DetailWidget> {
         setState(() => _reader = "unknown exception $e");
       }
     } on FormatException {
-      setState(() => () => new SnackBar(
+      setState(() => () => SnackBar(
             backgroundColor: Colors.red,
-            content: new Text("Scan Cancelled!"),
+            content: Text("Scan Cancelled!"),
           ));
     } catch (e) {
       setState(() => _reader = 'Unknown error: $e');
     }
   }
 
+
   _launchMapsUrl() async {
-    final url =
-        'https://www.google.com/maps/dir//Hotel+Sayaji,+Parshuram+Nagar,+Sayajiganj,+Vadodara,+Gujarat+390001/@22.305542,73.185483,20z/data=!4m8!4m7!1m0!1m5!1m1!1s0x395fcfb37f43d7b9:0xa4c63926d4354df0!2m2!1d73.1856699!2d22.3054179?hl=en';
+    final url = 'https://www.google.com/maps/dir//Hotel+Sayaji,+Parshuram+Nagar,+Sayajiganj,+Vadodara,+Gujarat+390001/@22.305542,73.185483,20z/data=!4m8!4m7!1m0!1m5!1m1!1s0x395fcfb37f43d7b9:0xa4c63926d4354df0!2m2!1d73.1856699!2d22.3054179?hl=en';
     if (await canLaunch(url)) {
       await launch(url);
     } else {
@@ -151,4 +171,3 @@ class _MyAppState extends State<DetailWidget> {
     }
   }
 }
-
